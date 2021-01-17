c5b41db0123292c61d54abfaaf7bb9d0e94ec3bb
Tahsin Masrur <tahsin86m@gmail.com>
2021-01-17 19:46:53 +0600

    Add about section

---
+++ app/src/main/java/com/zeronfinity/cpfy/view/LicenseFragment.kt
@@ -0,0 +1,51 @@
+package com.zeronfinity.cpfy.view
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.zeronfinity.core.entity.License
+import com.zeronfinity.core.logger.logD
+import com.zeronfinity.cpfy.databinding.FragmentLicenseBinding
+import com.zeronfinity.cpfy.view.adapter.AdapterLicenseList
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
+
+@AndroidEntryPoint
+class LicenseFragment : BaseFragment() {
+    private var _binding: FragmentLicenseBinding? = null
+    private val binding get() = _binding!!
+
+    @Inject
+    lateinit var adapterLicenseList: AdapterLicenseList
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        logD("onCreateView started")
+
+        _binding = FragmentLicenseBinding.inflate(inflater, container, false)
+
+        val cRomWhitelistLicense = License(
+            "CRomAppWhitelist",
+            "WanghongLin",
+            "Apache License 2.0",
+            "https://github.com/WanghongLin/CRomAppWhitelist/blob/master/LICENSE"
+        )
+
+        binding.rvLicenseList.adapter = adapterLicenseList
+        binding.rvLicenseList.layoutManager = LinearLayoutManager(binding.root.context)
+        binding.rvLicenseList.setHasFixedSize(true)
+
+        adapterLicenseList.refreshLicenseList(listOf(cRomWhitelistLicense))
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        logD("onViewCreated started")
+    }
+}
